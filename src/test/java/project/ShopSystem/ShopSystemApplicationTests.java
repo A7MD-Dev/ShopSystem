c5f3ca3781d88ff97a67f91216de87bcce867091
Ahmed Sayed <ahmedsayed.s1998@gmail.com>
2024-01-16 23:41:39 +0200

    Let's Start ^^

---
+++ src/test/java/project/ShopSystem/ShopSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package project.ShopSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
